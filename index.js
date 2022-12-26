be61ffc6bf81095ebf5f783d9b8fe8fa72069e41
sayoP <swj7852@gmail.com>
2022-12-27 04:12:04 +0900

    fix gitignore

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const connection = require('./config/db');
 
+// 라우터 import 
+const redirectRoute = require('./routes/redirect');
+const urlRoute = require('./routes/shorturl');
+
 connection.once('open', () => console.log('DB Connected'));
 connection.on('error', () => console.log('Error'));
 
-// body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// 라우터
+app.use('/', redirectRoute);
+app.use('/api/url', urlRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server started, listening PORT ${PORT}`));
