2ff8b89f2866f9b63c60c412481f5ba13a17deca
sayoP <swj7852@gmail.com>
2022-12-27 04:50:18 +0900

    check ejs Hello world

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+app.get('/', async (req, res) => {
+    res.render('view')
+  })
+  
 app.listen(process.env.PORT || 5000);
