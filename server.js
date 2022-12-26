9ff4cca2e3abee143bf98f50693cce31a5a2d7cd
sayoP <swj7852@gmail.com>
2022-12-27 05:26:41 +0900

    make view

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/urlmodel')
+
+mongoose.connect("mongodb+srv://sayoP:dnjfcjr41@cluster0.ibhq7m2.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
 
 app.set('view engine', 'ejs')
 app.get('/', async (req, res) => {
-    res.render('view')
+    const shortUrls = await ShortUrl.find()
+    res.render('view', { shortUrls: shortUrls })
   })
   
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+  
+    res.redirect('/')
+  })
+
 app.listen(process.env.PORT || 5000);
