370c8be90ebcf24e55f9a398c9041a8178d2516c
sayoP <swj7852@gmail.com>
2022-12-28 03:28:46 +0900

    add delete

--- index.js
+++ index.js
@@ -13,18 +13,20 @@ app.use(express.urlencoded({ extended: false }))
 app.get('/', async (req, res) => {
     const shortUrls = await ShortUrl.find()
     res.render('view', { shortUrls: shortUrls })
-  })
+})
   
 app.post('/shortUrls', async (req, res) => {
     await ShortUrl.create({ full: req.body.fullUrl })
     res.redirect('/')
-  })
-
+})
+app.post('/delete', async (req, res) => {
+    await ShortUrl.findOneAndRemove()
+    res.redirect('/')
+})
 app.get('/:shortUrl', async (req, res) => {
     const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
     if (shortUrl == null) return res.sendStatus(404)
     shortUrl.save()
-  
     res.redirect(shortUrl.full)
 })
 
